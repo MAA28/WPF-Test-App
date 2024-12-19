a95cedff5e277168e15e74c2f6fbc1824555c2be
MalteAschenbach <malte.aschenbach@hogrefe.com>
2024-12-19 12:27:32 +0100

    working reactivity

---
+++ WpfApp1/View/UserControls/MenuBar.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp1.View.UserControls;
+
+public partial class MenuBar : UserControl
+{
+    public MenuBar()
+    {
+        InitializeComponent();
+    }
+
+    private void TextBoxBase_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        Console.Out.WriteLine("TextBoxBase_OnTextChanged()");
+        Console.Out.WriteLine(tbInput.Text);
+        tbOutput.Text = tbInput.Text;
+    }
+}
